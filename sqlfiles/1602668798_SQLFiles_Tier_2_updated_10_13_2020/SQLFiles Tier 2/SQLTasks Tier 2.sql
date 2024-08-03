f68fe83470268f701e22c9dc451ca13e33ac3197
Krish <kgraghav@mtu.edu>
2024-08-02 20:42:28 -0700

    created new branch "development"

--- sqlfiles/1602668798_SQLFiles_Tier_2_updated_10_13_2020/SQLFiles Tier 2/SQLTasks Tier 2.sql
+++ sqlfiles/1602668798_SQLFiles_Tier_2_updated_10_13_2020/SQLFiles Tier 2/SQLTasks Tier 2.sql
@@ -34,7 +34,7 @@ exploring the data, and getting acquainted with the 3 tables. */
 /* QUESTIONS 
 /* Q1: Some of the facilities charge a fee to members, but some do not.
 Write a SQL query to produce a list of the names of the facilities that do. */
-
+ SELECT * FROM `Facilities` WHERE membercost>0
 
 /* Q2: How many facilities do not charge a fee to members? */
 
